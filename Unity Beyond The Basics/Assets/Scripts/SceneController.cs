398751a0032e10a268af63b1daf6e97edb35645a
ockley <vestergaard.karsten@gmail.com>
2019-10-23 15:19:22 +0200

    A litte further
    https://learn.unity.com/tutorial/working-with-groups-of-types/?courseId=5d0f57bcedbc2a0020cb53d3&projectId=5d0e3104edbc2a00212beef7&tab=materials#5d0e1d99edbc2a001f9a7794

---
+++ Unity Beyond The Basics/Assets/Scripts/SceneController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public List<Shape> gameShapes;
+    [SerializeField] public Dictionary<string, Shape> shapeDictionary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shapeDictionary = new Dictionary<string, Shape>();
+
+        foreach(Shape shape in gameShapes)
+        {
+            shapeDictionary.Add(shape.Name, shape);
+        }
+
+        
+    }
+
+    private void SetRedByName(string shapeName)
+    {
+        shapeDictionary[shapeName].SetColor(Color.red);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SetRedByName("Square");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SetRedByName("Circle");
+        }
+
+    }
+
+    private void FindExample()
+    {
+        Shape octagon = gameShapes.Find(s => s.Name == "Octagon");
+        octagon.SetColor(Color.red);
+    }
+
+    private void ListExample()
+    {
+        string[] shapes = { "circle", "square", "triangle", "octagon" };
+        List<string> moreShapes;
+
+        moreShapes = new List<string> { "circle", "square", "triangle", "octagon" };
+
+        moreShapes.Add("rectangle");
+        moreShapes.Insert(2, "Diamond");
+        moreShapes.Sort();
+
+        for (int i = 0; i < moreShapes.Count; i++)
+        {
+            moreShapes[i] = moreShapes[i].ToUpper();
+            Debug.Log(moreShapes[i]);
+        }
+    }
+}
